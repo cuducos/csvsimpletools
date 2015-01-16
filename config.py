04b61d529ec23348f0981d31452bbd6c754b65aa
Eduardo Cuducos <cuducos@gmail.com>
2015-01-16 14:27:33 +0000

    Minor changes

--- config.py
+++ config.py
@@ -3,12 +3,15 @@
 import os
 from decouple import config
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-WTF_CSRF_ENABLED = True
-SECRET_KEY = config('SECRET_KEY', default='')
-CSRF_ENABLED = True
 DEBUG = config('DEBUG', default=False, cast=bool)
-TITLE = 'CSS Simple Tools'
+SECRET_KEY = config('SECRET_KEY', default='')
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
+
+TITLE = 'CSV Simple Tools'
 LANGUAGES = {
     'en': 'English',
     'pt': 'Português'
